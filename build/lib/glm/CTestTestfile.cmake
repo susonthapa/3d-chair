ccb39c068b4e8d24b4249c08e965aeb64747faf5
Susan Thapa <susanthapa202@gmail.com>
2021-08-29 08:09:45 +0545

    clean build directory

--- build/lib/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/suson/Projects/3D Chair/lib/glm
-# Build directory: /home/suson/Projects/3D Chair/build/lib/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
